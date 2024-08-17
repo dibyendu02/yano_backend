5d0922cbf2f2f28a6fddbb6192e1906ecb1c55d7
Rohan Gope <rg2512@it.jgec.ac.in>
2024-08-17 14:20:26 +0530

    feat: Add MedicalHistory model
    The code changes introduce a new `MedicalHistory` model that includes various schemas for health conditions, family history, allergies, medicines, surgeries, vaccines, hospitalizations, and social history. This model is required for managing and storing medical history data for a user.
    
    Note: The recent user commits and repository commits do not provide any additional context for the purpose of these changes.

---
+++ src/models/MedicalHistory.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const HealthConditionsSchema = require("./MedicalHistories/healthConditions");
+const { FamilyHistorySchema } = require("./MedicalHistories/familyHistory");
+const { AllergiesSchema } = require("./MedicalHistories/allergies");
+const { MedicineSchema } = require("./MedicalHistories/medicine");
+const { SurgeriesSchema } = require("./MedicalHistories/surgeries");
+const { VaccineSchema } = require("./MedicalHistories/vaccine");
+const {
+  HospitalizationSchema,
+} = require("./MedicalHistories/hospitalizations");
+const { SocialHistorySchema } = require("./MedicalHistories/socialHistory");
+const { Schema } = mongoose;
+
+const MedicalHistorySchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "UserPatient",
+      required: true,
+    },
+    healthConditions: {
+      type: [HealthConditionsSchema],
+      required: false,
+    },
+    familyHistory: {
+      type: [FamilyHistorySchema],
+      required: false,
+    },
+    allergies: {
+      type: [AllergiesSchema],
+      required: false,
+    },
+    medicines: {
+      type: [MedicineSchema],
+      required: false,
+    },
+    surgeries: {
+      type: [SurgeriesSchema],
+      required: false,
+    },
+    vaccines: {
+      type: [VaccineSchema],
+      required: false,
+    },
+    hospitalizations: {
+      type: [HospitalizationSchema],
+      required: false,
+    },
+    socialHistory: {
+      type: [SocialHistorySchema],
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("MedicalHistory", MedicalHistorySchema); // Exporting the model
