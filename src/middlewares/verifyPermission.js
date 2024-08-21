50c48f3c07c110452bdb6084ddfd06360637ce34
Rohan Gope <rg2512@it.jgec.ac.in>
2024-08-22 00:06:24 +0530

    refactor: Add middleware for verifying user permissions
    This commit adds two new middleware functions, `verifyCanEditUser` and `verifyPermission`, to handle user permission verification. These middleware functions are responsible for checking the user's authentication token, retrieving the user's ID from the token, and then checking if the user has the necessary permissions to perform certain actions. The `verifyCanEditUser` middleware specifically checks if the user has the permission to edit user information, while the `verifyPermission` middleware sets the user's permission object on the request object for further use. These changes improve the security and authorization of the application.

---
+++ src/middlewares/verifyPermission.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const UserDoctor = require("../models/UserDoctor");
+
+const verifyPermission = async (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    let userId;
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) return res.status(403).json("Token is not valid");
+      userId = user.id;
+    });
+
+    const user = await UserDoctor.findById(userId);
+
+    if (!user) {
+      return res.status(403).json("You are not allowed to do that");
+    }
+
+    req.user = user.permission;
+
+    return next();
+  } else {
+    return res.status(401).json("you're not authenticated");
+  }
+};
+
+module.exports = {
+  verifyPermission,
+};
